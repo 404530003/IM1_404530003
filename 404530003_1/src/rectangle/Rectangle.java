ccc94017b5958a750c4191d79bce429083f3f257
HanselHu <apologize0616@gmail.com>
2016-03-08 23:12:42 +0800

    作業批改完成，寫做的很好！

--- 404530003_1/src/rectangle/Rectangle.java
+++ 404530003_1/src/rectangle/Rectangle.java
@@ -1,4 +1,14 @@
+/*
+[A]100
+[TA's advise]
+1.第13行印出的方法基本上沒有什麼問題, 但這樣寫對於程式的易讀性其實不是很好(如果今天你突然發現這一行某地方有錯可能會花上比較多的時間去debug)
+所以如果是我的話, 會建議將計算與印出來的字串分開寫.
+2.public是一個很開放性的修飾字, 若你想寫出比較精準的程式碼, 或許可以查查看public、protected、private與不寫之間的差異, 若全部都設定成public的確很好用
+但如果今天開發的是一個比較有資安性的程式, 這樣會安全嗎?
+3.你寫的很好.
+*/
 package rectangle;
+
 public class Rectangle 
 {
 	public static void main(String[] args) 
